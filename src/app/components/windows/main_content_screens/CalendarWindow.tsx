14a4ce827f41558993bee4500fac50ee2cd0b757
DevRoot <b4631119@gmail.com>
2026-07-09 05:10:48 +0600

    calendar

--- src/app/components/windows/main_content_screens/CalendarWindow.tsx
+++ src/app/components/windows/main_content_screens/CalendarWindow.tsx
@@ -16,7 +16,7 @@ const CalendarWindow = ({ windowName, setWindowsState, title = "TaskMate Calenda
             x={150}
             y={80}
             width="80vw" 
-            height="85vh"
+            height="80vh"
             title={title}
             onClose={() => setWindowsState((prev) => ({ ...prev, [windowName]: false }))}
         >
